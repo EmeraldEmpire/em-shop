3092b7c5aef68e32ef548a580e2877fb941bc81d
EmeraldEmpire <emeraldwhite25@gmail.com>
2025-08-21 10:55:54 +0800

    Add role to session through JWT Callback

--- auth.ts
+++ auth.ts
@@ -61,12 +61,33 @@ export const config = {
     async session({ session, user, trigger, token }: any) {
       // Set the user ID from the token
       session.user.id = token.sub
+      session.user.role = token.role
+      session.user.name = token.name
 
       // If there is an update, set the user name
       if (trigger === 'update') {
         session.user.name = user.name
       }
       return session
+    },
+
+    async jwt({ token, user, trigger, session }: any) {
+      // Assign user fields to token
+      if (user) {
+        token.role = user.role
+
+        // If user does not have name, use the email as name
+        if (user.name === 'NO_NAME') {
+          token.name = user.email.split('@')[0]
+
+          await prisma.user.update({
+            where: { id: user.id },
+            data: { name: token.name },
+          })
+        }
+      }
+
+      return token
     },
   },
 } satisfies NextAuthConfig
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,5 @@
 import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
-import { ZodError } from 'zod'
-import { Prisma } from '@prisma/client'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
